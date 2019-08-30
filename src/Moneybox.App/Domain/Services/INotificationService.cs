731617222fc5680643465bc64aa9823056fed9bc
cia95 <32906616+cia95@users.noreply.github.com>
2019-08-30 19:16:22 +0300

    Update INotificationService.cs

--- src/Moneybox.App/Domain/Services/INotificationService.cs
+++ src/Moneybox.App/Domain/Services/INotificationService.cs
@@ -1,9 +1,13 @@
-﻿namespace Moneybox.App.Domain.Services
+namespace Moneybox.App.Domain.Services
 {
     public interface INotificationService
     {
         void NotifyApproachingPayInLimit(string emailAddress);
 
         void NotifyFundsLow(string emailAddress);
+
+        void NotifyInsufficientFunds(string emailAddress);
+
+        void NotifySuccessfulTransaction(string emailAdress);
     }
 }
